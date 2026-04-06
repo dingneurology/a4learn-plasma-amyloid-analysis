13d32f4561e6512f63f60d45c8f2fc3740f42f36
dingneurology <102002303+dingneurology@users.noreply.github.com>
2026-04-06 20:56:44 +0800

    Create 01_data_prep_impute.R

---
+++ 01_data_prep_impute.R
@@ -0,0 +1,67 @@
+# Load source data
+demo <- read_csv("SUBJINFO.csv") %>%
+  select(SUBSTUDY, BID, AGEYR, SEX, RACE, EDCCNTU, ETHNIC,
+         APOEGN, APOEGNPRSNFLG, AAPOEGNPRSNFLG) %>%
+  mutate(APOE4_positive = ifelse(grepl("E4", APOEGN), 1, 0))
+
+plasma <- read_csv("biomarker_Plasma_Roche_Results.csv") %>%
+  select(BID, LBTESTCD, LABRESN)
+
+plasma_wide <- plasma %>%
+  pivot_wider(names_from = LBTESTCD, values_from = LABRESN) %>%
+  select(BID, GFAP, AMYLB40, AMYLB42, TPP181)
+
+ptau217 <- read_csv("biomarker_pTau217.csv")  %>%
+  select(BID, ORRESRAW)
+
+amyloid_diagnose <- read_csv("imaging_PET_VA.csv")
+
+# Merge analytic dataset
+all_data <- demo %>%
+  inner_join(amyloid_diagnose, by = c("BID", "SUBSTUDY")) %>%
+  inner_join(plasma_wide, by = "BID") %>%
+  inner_join(ptau217, by = "BID")
+
+# Data for imputation
+imp_data <- all_data %>%
+  select(SUBSTUDY, BID, AGEYR, SEX, RACE, EDCCNTU, ETHNIC,
+         APOE4_positive, pmod_suvr, GFAP, AMYLB40, AMYLB42, TPP181, ORRESRAW) %>%
+  mutate(
+    SUBSTUDY = as.factor(SUBSTUDY),
+    BID = as.factor(BID),
+    SEX = as.factor(SEX),
+    RACE = as.factor(RACE),
+    ETHNIC = as.factor(ETHNIC),
+    APOE4_positive = as.factor(APOE4_positive)
+  )
+
+# Initialize mice
+ini <- mice(imp_data, maxit = 0, printFlag = FALSE)
+meth <- ini$method
+pred <- ini$predictorMatrix
+
+# Do not impute IDs
+meth["BID"] <- ""
+meth["SUBSTUDY"] <- ""
+pred[, c("BID", "SUBSTUDY")] <- 0
+pred[c("BID", "SUBSTUDY"), ] <- 0
+
+# Methods by variable type
+meth[c("AGEYR", "EDCCNTU", "pmod_suvr","AMYLB40", "AMYLB42", "TPP181", "ORRESRAW")] <- "pmm"
+meth[c("SEX", "APOE4_positive", "ETHNIC")] <- "logreg"
+meth["RACE"] <- "polyreg"
+
+# Run imputation
+set.seed(123)
+imp <- mice(
+  imp_data,
+  m = 5,
+  method = meth,
+  predictorMatrix = pred,
+  maxit = 20,
+  printFlag = TRUE
+)
+
+complete_data <- complete(imp, 1)
+
+write_csv(complete_data, "output/complete_data.csv")
